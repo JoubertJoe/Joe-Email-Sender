fb7947b27191462a5091b448a038e40c5bb6af38
Joubert Joe Vieira Lellis <joubert.lellis@gmail.com>
2017-11-17 09:35:09 -0200

    Code
    First Commit !
    Still have some issues.

---
+++ src/gui/JoeEmailSender.java
@@ -0,0 +1,200 @@
+package gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import java.awt.FlowLayout;
+import java.awt.BorderLayout;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.layout.FormSpecs;
+import java.awt.GridLayout;
+import javax.swing.SpringLayout;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import javax.swing.ImageIcon;
+import java.awt.Color;
+import javax.swing.SwingConstants;
+import javax.swing.JTextArea;
+import javax.swing.JTextPane;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.awt.event.ActionEvent;
+import protocol.ProtocoloEmail;
+import javax.swing.JPasswordField;
+
+
+@SuppressWarnings("serial")
+public class JoeEmailSender extends JFrame {
+	private JTextField txtEmaildominiocom;
+	private JTextField txtAssuntoDoEmail;
+	private JButton btnEnviar;
+	private JTextArea textCorpoDoEmail;
+	private JTextField txtRemetente;
+	private JLabel lblSenha;
+	private JPasswordField passField;
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JoeEmailSender window = new JoeEmailSender();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	
+	
+	/**
+	 * Create the application.
+	 * 
+	 * Criando a aplicação.
+	 */
+	public JoeEmailSender() {
+		getContentPane().setBackground(Color.BLACK);
+		setTitle("Joe Email Sender");
+		setResizable(false);
+		createContent();
+		createActions();
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 * 
+	 * Inicializando conteudo da janela.
+	 */
+	private void initialize() {
+
+		this.setBounds(100, 100, 560, 494);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		this.setVisible(true);
+	}
+
+	/**
+	 * Creating the content of the frame.
+	 * 
+	 * Criando o conteúdo da janela.
+	 */
+	private void createContent() {
+
+		getContentPane().setLayout(null);
+		// Email subject - Assunto do email
+		txtAssuntoDoEmail = new JTextField();
+		txtAssuntoDoEmail.setText("ASSUNTO");
+		txtAssuntoDoEmail.setColumns(10);
+		txtAssuntoDoEmail.setBounds(120, 120, 344, 20);
+		getContentPane().add(txtAssuntoDoEmail);
+
+		// Email recipient - Destinatário do Email
+		txtEmaildominiocom = new JTextField();
+		txtEmaildominiocom.setBounds(120, 89, 344, 20);
+		txtEmaildominiocom.setText("joubert.lellis@gmail.com");
+		getContentPane().add(txtEmaildominiocom);
+		txtEmaildominiocom.setColumns(10);
+
+		JLabel lblDestino = new JLabel("Destino:");
+		lblDestino.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblDestino.setForeground(Color.WHITE);
+		lblDestino.setBounds(21, 91, 89, 16);
+		lblDestino.setFont(new Font("Verdana", Font.BOLD, 12));
+		getContentPane().add(lblDestino);
+
+		JLabel lblImagem = new JLabel("");
+		lblImagem.setHorizontalAlignment(SwingConstants.CENTER);
+		lblImagem.setIcon(new ImageIcon(
+				JoeEmailSender.class.getResource("/com/sun/javafx/scene/control/skin/caspian/images/vk-hide.png")));
+		lblImagem.setBounds(473, 57, 46, 32);
+		getContentPane().add(lblImagem);
+
+		JLabel lblAssunto = new JLabel("Assunto:");
+		lblAssunto.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblAssunto.setForeground(Color.WHITE);
+		lblAssunto.setFont(new Font("Verdana", Font.BOLD, 12));
+		lblAssunto.setBounds(21, 122, 89, 16);
+		getContentPane().add(lblAssunto);
+
+		JScrollPane scrollText = new JScrollPane();
+		scrollText.setBounds(41, 151, 480, 220);
+		getContentPane().add(scrollText);
+
+		textCorpoDoEmail = new JTextArea();
+		textCorpoDoEmail.setText("CORPO DO EMAIL");
+		textCorpoDoEmail.setSelectionColor(Color.GREEN);
+		textCorpoDoEmail.setSelectedTextColor(Color.BLACK);
+		textCorpoDoEmail.setDisabledTextColor(Color.LIGHT_GRAY);
+		textCorpoDoEmail.setCaretColor(Color.BLUE);
+		textCorpoDoEmail.setFont(new Font("Verdana", Font.PLAIN, 14));
+		textCorpoDoEmail.setForeground(Color.WHITE);
+		textCorpoDoEmail.setBackground(Color.DARK_GRAY);
+		scrollText.setViewportView(textCorpoDoEmail);
+
+		// Send Button - Botão Enviar
+		btnEnviar = new JButton("ENVIAR");
+		btnEnviar.setForeground(Color.WHITE);
+		btnEnviar.setFont(new Font("Verdana", Font.BOLD, 15));
+		btnEnviar.setBackground(Color.DARK_GRAY);
+		btnEnviar.setBounds(206, 395, 119, 41);
+		getContentPane().add(btnEnviar);
+		
+		JLabel lblRemetente = new JLabel("Remetente:");
+		lblRemetente.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblRemetente.setForeground(Color.WHITE);
+		lblRemetente.setFont(new Font("Verdana", Font.BOLD, 12));
+		lblRemetente.setBounds(21, 12, 89, 16);
+		getContentPane().add(lblRemetente);
+		
+		txtRemetente = new JTextField();
+		txtRemetente.setForeground(Color.WHITE);
+		txtRemetente.setBackground(Color.BLACK);
+		txtRemetente.setText("aluno@ailton.eng.br");
+		txtRemetente.setColumns(10);
+		txtRemetente.setBounds(120, 12, 344, 20);
+		getContentPane().add(txtRemetente);
+		
+		lblSenha = new JLabel("Senha:");
+		lblSenha.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblSenha.setForeground(Color.WHITE);
+		lblSenha.setFont(new Font("Verdana", Font.BOLD, 12));
+		lblSenha.setBounds(21, 39, 89, 16);
+		getContentPane().add(lblSenha);
+		
+		passField = new JPasswordField();
+		passField.setForeground(Color.WHITE);
+		passField.setBackground(Color.BLACK);
+		passField.setText("iftmitba1");
+		passField.setColumns(10);
+		passField.setBounds(120, 39, 344, 20);
+		getContentPane().add(passField);
+	}
+
+	private void createActions() {
+		btnEnviar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					ProtocoloEmail protocolSMTP = new ProtocoloEmail();
+					protocolSMTP.enviar(textCorpoDoEmail.getText(),txtAssuntoDoEmail.getText(), "aluno@ailton.eng.br",txtEmaildominiocom.getText(),"smtp.ailton.eng.br", String.valueOf(passField.getPassword()));
+					JOptionPane.showMessageDialog(btnEnviar, protocolSMTP.log);
+				} catch (UnknownHostException e) {
+					// TODO Auto-generated catch block
+					JOptionPane.showMessageDialog(btnEnviar, "Erro : " + e + "/n");
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
---
+++ src/protocol/ProtocoloEmail.java
@@ -0,0 +1,181 @@
+package protocol;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.util.Base64;
+import java.util.Locale;
+
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+/**
+ * 
+ * @author Joubert Joe Vieira Lellis
+ *
+ */
+public class ProtocoloEmail {
+
+	public String log;
+	
+	
+	/**
+	 * 
+	 * @param emailBody - Corpo do email - Body of the Email
+	 * @param emailSubject - Assunto do EMail - Subject of the Email
+	 * @param emailSender - Remetente do email - Email Sender
+	 * @param emailRecipient - Destinatário do Email - Email Recipiente
+	 * @param emailHost - HOST (Gmail, Hotmail, Yahoo)
+	 * @throws IOException
+	 * @throws UnknownHostException
+	 */
+	public void enviar( String emailBody, String emailSubject, String emailSender,String emailRecipient,String emailHost, String password) throws IOException, UnknownHostException{
+		
+		SMTP mail = new SMTP(emailHost);
+		// Protocolo - Protocol (Simple Mail Transpor Protocol)
+		
+		if (mail != null) {
+			if (mail.send(emailSubject , emailBody, emailSender, emailRecipient, password)) {
+				log = ("Email Enviado \n Mail Sent");
+			} else {
+				log = ("Falha ao conectar com servidor SMTP \n Connect to SMTP server failed!");
+			}
+		}
+		System.out.println("Pronto. \n Done.");
+	}
+	
+/**
+ * 
+ * @author Joubert Joe Vieira Lellis
+ *
+ */
+	static class SMTP {
+		private final static int SMTP_PORT = 587; // Porta padrão SMTP - Default Port SMTP
+		InetAddress mailHost;
+		InetAddress localhost;
+		BufferedReader in;
+		PrintWriter out;
+
+		/**
+		 * 
+		 * @param	host Host a ser usado - Host to be used. Ex: (gmail, hotmail, yahoo, etc...)
+		 * @throws	UnknownHostException Tratamento de exceção de Host - Host Exception
+		 */
+		public SMTP(String host) throws UnknownHostException {
+			mailHost = InetAddress.getByName(host);
+			localhost = InetAddress.getLocalHost();
+			System.out.println("mailhost = " + mailHost);
+			System.out.println("localhost= " + localhost);
+			System.out.println("Contrutor SMTP pronto. /n SMTP constructor done\n");
+		}
+		
+		/**
+		 * 
+		 * @param msgEmail		Mensagem contendo Assunto e corpo de email - Mensage includind subject and body of the email.
+		 * @param from			Quem vai enviar o email - Who gonna send that Email.
+		 * @param to			Quem está recebendo o email - Who gonna recieve that email.
+		 * @return				Booleana para saber se tudo ocorreu bem - Boolean to check if everythings is right.
+		 * @throws IOException	Possíveis erros - Possible errors.
+		 */
+		public boolean send(String msgAssunto, String msgEmail, String from, String to, String pass) throws IOException {
+		//	SSLSocket smtpSocket;
+			Socket smtpSocket;
+			InputStream inn;
+			OutputStream outt;
+			
+		// Criptografia 
+			String user = Base64.getEncoder().encodeToString(from.getBytes("utf-8"));
+			pass = Base64.getEncoder().encodeToString(pass.getBytes("utf-8"));
+			
+		//	smtpSocket = (SSLSocket) ((SSLSocketFactory) SSLSocketFactory.getDefault()).createSocket(mailHost, SMTP_PORT);
+			smtpSocket = new Socket(mailHost, SMTP_PORT);
+			
+			if (smtpSocket == null) {
+				return false;
+			}
+			inn = smtpSocket.getInputStream();
+			outt = smtpSocket.getOutputStream();
+			in = new BufferedReader(new InputStreamReader(inn));
+			out = new PrintWriter(new OutputStreamWriter(outt), true);
+			if (inn == null || outt == null) {
+				System.out.println("Failed to open streams to socket.");
+				return false;
+			}
+			String initialID = in.readLine();
+			System.out.println(initialID);
+			System.out.println("HELO ailton.eng.br");
+			out.println("HELO ailton.eng.br");
+			out.flush();
+			String welcome = in.readLine();
+			System.out.println(welcome);
+			System.out.println("AUTH LOGIN");
+			out.println("AUTH LOGIN");
+			out.flush();
+			out.println(user);
+			out.flush();
+			System.out.println(user);
+			out.println(pass);
+			out.flush();
+			System.out.println(pass);
+			
+			System.out.println("MAIL From:<" + from + ">");
+			out.println("MAIL FROM:<" + from + ">");
+			out.flush();
+			
+			String senderOK = in.readLine();
+			System.out.println(senderOK);
+			
+			System.out.println("RCPT TO:<" + to + ">");
+			out.println("RCPT TO:<" + to + ">");
+			out.flush();
+			String recipientOK = in.readLine();
+			System.out.println(recipientOK);
+			
+			System.out.println("DATA");
+			out.println("DATA");
+			out.flush();
+			
+			out.println("Subject: " +msgAssunto);
+			out.flush();
+			System.out.println(msgAssunto);
+			
+			out.println(msgEmail);
+			out.flush();
+			System.out.println(msgEmail);
+			
+			System.out.println(".");
+			out.println(".");
+			out.flush();
+			
+			String acceptedOK = in.readLine();
+			String resposta;
+			
+			System.out.println("QUIT");
+			out.println("QUIT");
+			out.flush();
+			
+			System.out.println(acceptedOK);
+			do{
+			   resposta = in.readLine();
+			   
+			   System.out.println(resposta);
+			}while(resposta != null); 
+			 
+			
+			return true;
+		}
+
+	}
+
+}
